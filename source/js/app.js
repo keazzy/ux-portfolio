387663aa948fe7987f37472a87586fb49cea6612
Balogun Abdulazeez Adeleke <52777282+keazzy@users.noreply.github.com>
2020-05-26 01:05:13 +0100

    dark mode works now

--- source/js/app.js
+++ source/js/app.js
@@ -10,19 +10,52 @@ window.sr = ScrollReveal();
 // });
 
 //DarkMode
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+var menuToggleButton = document.querySelector("button.toggle");
+var menuTarget = document.querySelector(".side-menu");
+var menuBlock = document.querySelector("menu");
 
-function switchTheme(e){
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else{
-        document.documentElement.setAttribute('data-theme', light);
-    }
+function toggleMenuVisibility(){
+	menuTarget.classList.toggle("side-menu--active");
+	animateCSS(".menu", "fadeIn");
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+function animateCSS(element, animationName, callback) {
+    var node = document.querySelector(element)
+    node.classList.add('animated', animationName)
 
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
+menuToggleButton.addEventListener('click', toggleMenuVisibility);
+
+
+
+
+// Toggle dark/light mode 
+var wrapper = document.querySelector("body");
+var darkModeToggle = document.querySelector(".dark-mode-button");
+
+function toggleSunMoon() {
+	wrapper.classList.toggle("dark");
+	if (darkModeToggle.name == "moon-outline") {
+		darkModeToggle.name = "sunny";
+	} else {
+		darkModeToggle.name = "moon-outline";
+	}
+}
+
+function toggleDarkMode() {
+	toggleSunMoon();
+}
+
+darkModeToggle.addEventListener("click", toggleDarkMode);
 
 
 //
